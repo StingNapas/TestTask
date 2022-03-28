f5701689d1e57417e2b36431324a0f8acd77bf95
Сергей <2sting2@mail.ru>
2022-03-28 10:11:30 +0300

    Swap

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -6,8 +6,24 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Commit1");
+            Console.WriteLine("Введите переменную 1");
+            var variable1Str = Console.ReadLine();
+            Int32.TryParse(variable1Str, out var variable1);
+            Console.WriteLine("Введите переменную 2");
+            var variable2Str = Console.ReadLine();
+            Int32.TryParse(variable2Str, out var variable2);
+            Console.WriteLine("Переменные до вызова метода Swap");
+            Console.WriteLine($"Переменная 1 = {variable1}, переменная 2 = {variable2}");
+            Swap(ref variable1, ref variable2);
+            Console.WriteLine("Переменные после вызова метода Swap");
+            Console.WriteLine($"Переменная 1 = {variable1}, переменная 2 = {variable2}");
+        }
+
+        static void Swap(ref int variable1, ref int variable2)
+        {
+            var temp = variable1;
+            variable1 = variable2;
+            variable2 = temp;
         }
     }
 }
